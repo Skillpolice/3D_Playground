8a2bc9567ed17c69e8987433ce9416303a3070a5
Denis Romanovskiy <53230856+Skillpolice@users.noreply.github.com>
2021-01-28 20:54:25 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float rotationSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+
+        //rb.velocity = transform.forward * inputVertical * speed; 
+        rb.AddForce(transform.forward * inputVertical * speed);
+
+
+        transform.RotateAround(transform.position, Vector3.up, inputHorizontal * rotationSpeed);
+
+    }
+}
